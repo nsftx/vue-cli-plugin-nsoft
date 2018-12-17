2d27555869ea3278b96afab0257c91d370e67c9f
Marko <vucina.marko@nsoft.com>
2018-12-17 18:52:24 +0100

    Update store in generatore #9

--- generator/template/default/src/store/index.js
+++ generator/template/default/src/store/index.js
@@ -1,19 +1,7 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import actions from './actions';
-import getters from './getters';
-import modules from './modules';
-import mutations from './mutations';
-import plugins from './plugins';
-import state from './state';
+import storeConfig from './storeConfig';
 
 Vue.use(Vuex);
 
-export default new Vuex.Store({
-  actions,
-  getters,
-  modules,
-  mutations,
-  plugins,
-  state,
-});
+export default new Vuex.Store(storeConfig);
