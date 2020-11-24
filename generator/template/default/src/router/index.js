4af0a4a797c1a9b05dd08d965e59b462b815d5c1
Alen Sendro <sendro.alen@nsoft.com>
2020-11-24 10:20:05 +0100

    Update router for vue3
     - Ref T95559

--- generator/template/default/src/router/index.js
+++ generator/template/default/src/router/index.js
@@ -1,11 +1,7 @@
-import Vue from 'vue';
-import Router from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 
-Vue.use(Router);
-
-export default new Router({
-  mode: 'history',
-  base: process.env.BASE_URL,
+export default createRouter({
+  history: createWebHashHistory(),
   routes: [
     {
       path: '/',
